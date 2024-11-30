e6eef1f841854e53e631fbfb10184f7f31ab8697
Arunima Saha <arunimasaha2002@gmail.com>
2024-12-01 01:19:36 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import speech_recognition as sr
+import os
+import webbrowser
+import datetime
+import random
+import numpy as np
+import pyttsx3
+
+
+chatStr = ""
+def chat(query):
+    global chatStr
+    print(chatStr)
+    chatStr += f"Harry: {query}\n Jarvis: "
+   
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from Jarvis"
+def say(text):
+    engine=pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+if __name__ == '__main__':
+    print('Welcome to Jarvis A.I')
+    say("Jarvis A.I")
+    while True:
+        print("Listening...")
+        query = takeCommand()
+        
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"],]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
+        
+        if "open music" in query:
+            musicPath = "/Users/harry/Downloads/downfall-21371.mp3"
+            os.system(f"open {musicPath}")
+
+        elif "the time" in query:
+            musicPath = "/Users/harry/Downloads/downfall-21371.mp3"
+            hour = datetime.datetime.now().strftime("%H")
+            min = datetime.datetime.now().strftime("%M")
+            say(f"Sir time is {hour} bajke {min} minutes")
+
+        elif "open facetime".lower() in query.lower():
+            os.system(f"open /System/Applications/FaceTime.app")
+
+        elif "open pass".lower() in query.lower():
+            os.system(f"open /Applications/Passky.app")
+
+        elif "Jarvis Quit".lower() in query.lower():
+            exit()
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+        else:
+            print("Chatting...")
+            chat(query)
